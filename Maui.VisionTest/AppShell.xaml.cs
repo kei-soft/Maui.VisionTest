8d62f8ca086157130abc58edd1e9190c0099eda8
kjun <keisoft.kjun@gmail.com>
2023-07-05 00:21:27 +0900

    초기 커밋.

---
+++ Maui.VisionTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.VisionTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui.VisionTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BarcodeScanner.Mobile;
+
+namespace Maui.VisionTest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        BarcodeScanner.Mobile.Methods.AskForRequiredPermission();
+    }
+
+    private void Camera_OnDetected(object sender, BarcodeScanner.Mobile.OnDetectedEventArg e)
+    {
+        List<BarcodeResult> barcodeResults = e.BarcodeResults;
+
+        string result = string.Empty;
+        for (int i = 0; i < barcodeResults.Count; i++)
+        {
+            result += $"Type : {barcodeResults[i].BarcodeType}, Value : {barcodeResults[i].DisplayValue}{Environment.NewLine}";
+        }
+
+        Dispatcher.Dispatch(async () =>
+        {
+            await DisplayAlert("Result", result, "OK");
+            // If you want to start scanning again
+            Camera.IsScanning = true;
+        });
+    }
+}
+
